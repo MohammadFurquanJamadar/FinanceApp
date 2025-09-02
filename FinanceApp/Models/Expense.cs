555fdb40ebb53d4b02d12225724fa241dbdadc25
Mohammad Furquan Jamadar <mmmj131310@gmail.com>
2025-09-02 11:43:19 +0530

    Initial Commit

---
+++ FinanceApp/Models/Expense.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApp.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount cannot be negative")]
+        public double Amount { get; set; }
+
+        [Required]
+        public string? Category { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+    }
+}
